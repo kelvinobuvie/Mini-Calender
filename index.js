687c54d921d49846a4d2779fa4e065fff566eba9
kelvinobuvie <kelvinobuvie@gmil.com>
2023-06-16 19:12:47 +0100

    added dynamic code

---
+++ index.js
@@ -0,0 +1,20 @@
+const monthNameEL = document.getElementById("month-name");
+const dayNameEl = document.getElementById("day-name");
+const dayNumEl = document.getElementById("day-number");
+const yearEl = document.getElementById("year");
+
+const date = new Date();
+const month = date.getMonth
+monthNameEL.innerText = date.toLocaleDateString("en", {
+    month: "long"
+})
+
+
+dayNameEl.innerText = date.toLocaleDateString("en", {
+    weekday: "long"
+})
+
+dayNumEl.innerText = date.getDate()
+
+yearEl.innerText = date.getFullYear()
+
